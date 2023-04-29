6727ca5e4b9a791ebe0b95ced4a9347dea4adf0d
54padmakshi <115454581+54padmakshi@users.noreply.github.com>
2023-04-30 01:06:14 +0530

    updated

--- src/MyCartPage.js
+++ src/MyCartPage.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 
 function MyCartPage() {
+  const showmsg = ()=>{
+    document.getElementById('msg').textcontent = "Success ! The items have been checkout";
+   
+  }
   return (
     <div>
      <h1 style={{textAlign:"center"}}> My Cart </h1>
@@ -13,13 +17,14 @@ function MyCartPage() {
              <p></p>
            <button> Remove from Cart </button>
            </div>   
-           <div style={{textAlign : "center",width : "30%" , border:"1px solid black",backgroundColor:"black",color:"white",marginRight:"20px"}}>
-               <h1>Checkout List</h1> 
-            </div>
-        
+           <div style={{textAlign : "center",width : "30%" ,height:"500px", border:"1px solid black",backgroundColor:"black",color:"white",marginRight:"20px"}}>
+               <h1>Checkout List</h1>
+               <button onClick={showmsg}>Click to Checkout</button> 
+              </div>
+              
          </div>
     
-    
+         <div id='msg' style={{color:"red", backgroundColor:"red"}}> </div>
     </div>
   )
 }
